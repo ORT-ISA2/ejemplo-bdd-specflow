3a357ccd2d5760e88918ce77489fe60e2ffa872a
cpfontan <carina.fontan@fi365.ort.edu.uy>
2022-05-15 23:56:41 -0300

    Add Calculator project

---
+++ Calculator/CalculatorTests/Features/Substraction.feature
@@ -0,0 +1,11 @@
+﻿Feature: Substraction
+	In order to avoid silly mistakes
+	As a math novice
+	I want to be told the substraction of two numbers
+	
+@mytag
+Scenario: Substract two numbers
+	Given the minuend is 120
+	And the subtrahend is 70
+	When the minuend is substract from the subtrahend
+	Then the substraction result should be 50
---
+++ Calculator/CalculatorTests/Steps/SubstractionStepDefinitions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using CalculatorExample;
+using TechTalk.SpecFlow;
+using FluentAssertions;
+
+namespace CalculatorTests.Steps
+{
+    [Binding]
+    public sealed class SubstractionStepDefinitions
+    {
+        private readonly ScenarioContext _scenarioContext;
+        private readonly Calculator _calculator = new Calculator();
+        private int _result;
+
+        public SubstractionStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given("the minuend is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            _calculator.FirstNumber = number;
+        }
+
+        [Given("the subtrahend is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+            _calculator.SecondNumber = number;
+        }
+
+        [When("the minuend is substract from the subtrahend")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            _result = _calculator.Substract();
+        }
+
+        [Then("the substraction result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            _result.Should().Be(result);
+        }
+
+    }
+}
